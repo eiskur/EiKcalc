3fb16b6fe7467e8055c7fc091a837325158d9b18
eiskur <kurteisendle@gmail.com>
2026-05-25 08:46:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eikcalc-v2';
+const CACHE = 'eikcalc-v3';
 const FILES = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
